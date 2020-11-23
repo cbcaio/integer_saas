f77670734e68b1837e48a11d1e01283eed7447dc
Caio Ceccon Bolognani <caio.bolognani@gmail.com>
2020-11-22 21:05:37 -0300

    fix(terraform): adding missing permission to execute lambda from api gateway

--- infra/service/main.tf
+++ infra/service/main.tf
@@ -85,6 +85,14 @@ resource "aws_iam_role_policy" "attach_vpc" {
   policy = data.template_file.aws_lambda_vpc_role_policy.rendered
 }
 
+resource "aws_lambda_permission" "allow_api_gateway_trigger" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*/*"
+}
+
 # --------------- #
 # Lambda Function #
 # --------------- #
